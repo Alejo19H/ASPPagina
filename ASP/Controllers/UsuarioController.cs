36175d68d6ceb73a478a76bc372510e73d3e60a0
Alejo19H <alejo09an.10@gmail.com>
2021-09-28 23:07:35 -0500

    Cargue masivo de tabla de proveedor y usuario

--- ASP/Controllers/UsuarioController.cs
+++ ASP/Controllers/UsuarioController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -176,6 +177,75 @@ namespace ASP.Controllers
         {
             FormsAuthentication.SignOut();
             return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult UploadCSV()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadCSV(HttpPostedFileBase fileForm)
+        {
+            try
+            {
+
+                //string para guardar la ruta
+                string filePath = string.Empty;
+
+                //condicion para saber si el archivo llego
+                if (fileForm != null)
+                {
+                    //ruta de la carpeta que guardara el archivo
+                    string path = Server.MapPath("~/Uploads/");
+
+                    //condicion para saber si la carpeta uploads existe
+                    if (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+
+                    //obtener el nombre del archivo
+                    filePath = path + Path.GetFileName(fileForm.FileName);
+
+                    //obtener la extension del archivo
+                    string extension = Path.GetExtension(fileForm.FileName);
+
+                    //guardar el archivo
+                    fileForm.SaveAs(filePath);
+
+                    string csvData = System.IO.File.ReadAllText(filePath);
+
+                    foreach (string row in csvData.Split('\n'))
+                    {
+                        if (!string.IsNullOrEmpty(row))
+                        {
+                            var newUsuario = new usuario
+                            {
+                                nombre = row.Split(',')[0],
+                                apellido = row.Split(',')[1],
+                                fecha_nacimiento = Convert.ToDateTime(row.Split(',')[2]),
+                                email = row.Split(',')[3],
+                                password = row.Split(',')[4]
+                            };
+
+                            using (var db = new inventario2021Entities())
+                            {
+                                db.usuario.Add(newUsuario);
+                                db.SaveChanges();
+                            }
+                        }
+                    }
+                }
+
+                return View();
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
         }
     }
 }
